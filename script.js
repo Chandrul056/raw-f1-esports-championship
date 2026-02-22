c9641946e8ebe60753c5b54c296bd7e931ec035f
Chandru L <47409590+Chandrul056@users.noreply.github.com>
2026-02-22 20:53:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ const CONFIG = {
         { name: "R2 – China", csv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSDjTgVEjSqmQ2kpcAggNlOfCf_ECrq8yO3DzIcyyQjXs0fj1L9mFaM1Td1AwNJIKiaI6FVW7E-oIh0/pub?gid=1510901277&single=true&output=csv" },
         { name: "R3 – Japan", csv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSDjTgVEjSqmQ2kpcAggNlOfCf_ECrq8yO3DzIcyyQjXs0fj1L9mFaM1Td1AwNJIKiaI6FVW7E-oIh0/pub?gid=1358920533&single=true&output=csv" },
         { name: "R4 - Saudi Arabia", csv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSDjTgVEjSqmQ2kpcAggNlOfCf_ECrq8yO3DzIcyyQjXs0fj1L9mFaM1Td1AwNJIKiaI6FVW7E-oIh0/pub?gid=1615900483&single=true&output=csv" },
+        { name: "R5 - Great Britain", csv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSDjTgVEjSqmQ2kpcAggNlOfCf_ECrq8yO3DzIcyyQjXs0fj1L9mFaM1Td1AwNJIKiaI6FVW7E-oIh0/pub?gid=1400816626&single=true&output=csv" },
     ],
 
     // Home page "Next Race" card
@@ -451,3 +452,4 @@ $("#raceFilter")?.addEventListener("input", () => {
         setText("lastUpdated", "Failed to load data (check CSV links in script.js).");
     }
 })();
+
