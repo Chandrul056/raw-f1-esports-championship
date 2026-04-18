d01c26da7f7b4813e5b17de08312dd5064909612
Chandru L <47409590+Chandrul056@users.noreply.github.com>
2026-04-18 23:36:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ const CONFIG = {
         { name: "R7 - Netherlands", csv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSDjTgVEjSqmQ2kpcAggNlOfCf_ECrq8yO3DzIcyyQjXs0fj1L9mFaM1Td1AwNJIKiaI6FVW7E-oIh0/pub?gid=2138646510&single=true&output=csv" },
         { name: "R8 - Brazil", csv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSDjTgVEjSqmQ2kpcAggNlOfCf_ECrq8yO3DzIcyyQjXs0fj1L9mFaM1Td1AwNJIKiaI6FVW7E-oIh0/pub?gid=6299775&single=true&output=csv" },
         { name: "R9 - Las Vegas", csv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSDjTgVEjSqmQ2kpcAggNlOfCf_ECrq8yO3DzIcyyQjXs0fj1L9mFaM1Td1AwNJIKiaI6FVW7E-oIh0/pub?gid=815539015&single=true&output=csv" },
+        { name: "R10 - Qatar", csv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSDjTgVEjSqmQ2kpcAggNlOfCf_ECrq8yO3DzIcyyQjXs0fj1L9mFaM1Td1AwNJIKiaI6FVW7E-oIh0/pub?gid=1769117414&single=true&output=csv" },
     ],
 
     // Home page "Next Race" card
